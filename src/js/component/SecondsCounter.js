b0a97f5e5c7465ce11e734e264bc3a2bd46d9fed
Ginajohana <124780437+Ginajohana@users.noreply.github.com>
2023-05-05 19:32:28 +0000

    subo ejercicio

---
+++ src/js/component/SecondsCounter.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+
+//include images into your bundle
+
+//create your first component
+function SecondsCounter() {
+	return (
+		<div className="Counter">
+		<div className="calendar"><i class="far fa-clock"></i>
+			</div>
+			<div className="four">0</div>
+			<div className="three">0</div>
+			<div className="two">0</div>
+			<div className="one">0</div>
+
+		</div>
+	);
+}
+
+export default SecondsCounter;
