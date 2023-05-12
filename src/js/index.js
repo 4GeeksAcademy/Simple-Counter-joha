975046873a7a74d2c40a12577f05730b66f482d5
Ginajohana <124780437+Ginajohana@users.noreply.github.com>
2023-05-12 11:38:48 +0000

    actualizo proyecto

--- src/js/index.js
+++ src/js/index.js
@@ -3,41 +3,49 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
 
-
-//import your own components
 function SecondsCounter(props) {
-    return (<div className="bigCounter">;
-        <div className="calendar">
+  return (
+    <div className="bigCounter">
+      <div className="calendar">
         <i className="far fa-clock"></i>
-        </div>
-        <div className="four">{props.digitFour}</div>
-        <div className="three">{props.digitThree}</div>
-        <div className="two">{props.digitTwo}</div>
-        <div className="one">{props.digitOne}</div>
-    </div>);
-
-};
-SecondsCounter.propTypes = {
-    digitFour: PropTypes.number,
-    digitThree: PropTypes.number,
-    digitTwo:PropTypes.number,
-    digitOne:PropTypes.number
+      </div>
+      <div className="four">{props.digitFour}</div>
+      <div className="three">{props.digitThree}</div>
+      <div className="two">{props.digitTwo}</div>
+      <div className="one">{props.digitOne}</div>
+    </div>
+  );
 }
 
+SecondsCounter.propTypes = {
+  digitFour: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitOne: PropTypes.number,
+};
+
 let counter = 0;
-setInterval(function(){
-    const four = Math.floor(counter/1000);
-    const three = Math.floor(counter/100);
-    const two = Math.floor(counter/10);
-    const one = Math.floor(counter/1);
-    console.log(four, three, two, one);
-    //render your react application
-    counter++;
-        ReactDOM.render(<SecondsCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four}/>, document.querySelector("#app")
-        );
-    },1000);
+setInterval(function () {
+  const four = Math.floor(counter / 1000);
+  const three = Math.floor((counter / 100) % 10);
+  const two = Math.floor((counter / 10) % 10);
+  const one = Math.floor(counter % 10);
+  console.log(four, three, two, one);
 
+  ReactDOM.render(
+    <SecondsCounter
+      digitOne={one}
+      digitTwo={two}
+      digitThree={three}
+      digitFour={four}
+    />,
+    document.querySelector("#app")
+  );
 
+  counter++;
+  if (counter > 9999) {
+    counter = 0;
+  }
+}, 1000);
